62971bad61fcd74352229fac6e51fa73ba902079
a.mironyuk <sochemironiuk@yandex.ru>
2022-11-16 17:28:29 +0300

    feat:start creating classes

--- ewm-service/src/main/java/ru/practicum/user/model/User.java
+++ ewm-service/src/main/java/ru/practicum/user/model/User.java
@@ -6,6 +6,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
@@ -17,9 +21,14 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
+    @PositiveOrZero
     private Long id;
     @Column(name = "name", nullable = false)
+    @NotNull
     private String name;
     @Column(name = "email", nullable = false, unique = true,  length = 512)
+    @Size(min = 1, max = 512)
+    @Email
+    @NotNull
     private String email;
 }
